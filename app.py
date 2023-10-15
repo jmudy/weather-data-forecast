ba98b41bb38768d20fbe2c656daf4e1aa3f438fa
jmudy <62428273+jmudy@users.noreply.github.com>
2023-10-15 18:42:47 +0200

    ligeras mejoras

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from aemet import Aemet, Estacion
+from aemet import Aemet
 import pandas as pd
 from config import API_KEY, FECHAINI, FECHAFIN
 
@@ -8,11 +8,16 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-st.markdown("""
-<style>
-    section.main > div {max-width:70rem}
-</style>
-""", unsafe_allow_html=True)
+import warnings
+warnings.simplefilter("ignore", category=FutureWarning)
+
+st.markdown(
+	"""
+	<style>
+		section.main > div {max-width:70rem}
+	</style>
+	""", unsafe_allow_html=True
+	)
 
 st.title('Weather data forecast in Valencia')
 
@@ -22,52 +27,55 @@ st.sidebar.title('Settings')
 st.sidebar.markdown('---')
 station = st.sidebar.selectbox('**Select station for prediction**', stations, index=None)
 st.sidebar.markdown('---')
-n_years = st.sidebar.slider('**Years of prediction:**',min_value=1, max_value=4, value=2)
-period = n_years * 365
+n_days = st.sidebar.slider('**Days of prediction:**',min_value=1, max_value=60, value=40)
+
+if station:
+	forecast = st.sidebar.button('Make forecast')
 
 def plot_raw_data():
 	fig = go.Figure()
 	fig.add_trace(go.Scatter(x=df['fecha'], y=df['tmed'], name="tmed"))
-	fig.add_trace(go.Scatter(x=df['fecha'], y=df['tmax'], name="tmax"))
-	fig.add_trace(go.Scatter(x=df['fecha'], y=df['tmin'], name="tmin"))
 	fig.layout.update(title_text='Time Series data', xaxis_rangeslider_visible=True)
 	st.plotly_chart(fig)
 
-if station:
-
-	data_load_state = st.text('Loading data...')
-
+@st.cache_data
+def load_data():
 	aemet = Aemet(api_key=API_KEY)
-	estaciones = Estacion.get_estaciones(api_key=API_KEY)
-
 	data = aemet.get_valores_climatologicos_diarios(fechaini=FECHAINI, fechafin=FECHAFIN, estacion=station)
 	df = pd.DataFrame(data)
-	numeric_columns = ['tmed','prec','tmin','tmax','velmedia','racha','sol','presMax','presMin']
+	df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
+	numeric_columns = ['tmed','tmin','tmax','velmedia','racha','sol','presMax','presMin']
 	for column in numeric_columns:
-		df[column] = df[column].str.replace(',', '.')	
+		df[column] = df[column].str.replace(',', '.').astype(float)
+	return df
 
+if forecast:
+
+	data_load_state = st.text('Loading data...')
+	df = load_data()
 	data_load_state.text('Loading data... done!')
-
-	st.subheader('Raw data')
-	st.write(df.tail(10))
-		
-	plot_raw_data()
 
 	df_train = df[['fecha','tmed']]
 	df_train = df_train.rename(columns={"fecha": "ds", "tmed": "y"})
 
-	m = Prophet()
-	m.fit(df_train)
-	future = m.make_future_dataframe(periods=period)
-	forecast = m.predict(future)
+	st.subheader('Raw data')
+	st.write(df.tail(10))
+
+	plot_raw_data()
+
+	model = Prophet()
+	model.fit(df_train)
+	future = model.make_future_dataframe(periods=n_days)
+	forecast = model.predict(future)
 
 	st.subheader('Forecast data')
-	st.write(forecast.tail(10))
+	st.write(forecast[['ds', 'trend_lower', 'trend', 'trend_upper', 'yhat_lower', 'yhat', 'yhat_upper']].tail(10))
 		
-	st.write(f'Forecast plot for {n_years} years')
-	fig1 = plot_plotly(m, forecast)
+	st.write(f'Forecast plot for {n_days} days')
+	fig1 = plot_plotly(model, forecast)
 	st.plotly_chart(fig1)
 
+	# Sustituir esto por una tabla resumen con las métricas más relevantes
 	st.subheader("Forecast components")
-	fig2 = m.plot_components(forecast)
+	fig2 = model.plot_components(forecast)
 	st.write(fig2)
