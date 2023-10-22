ae04b961a11c50a18868e2fc43675eb4617d62fa
jmudy <62428273+jmudy@users.noreply.github.com>
2023-10-22 23:42:30 +0200

    Selector ajuste hiperparametros
    Se ha añadido un checkbox para decidir si se seleccionan los hiperparámetros óptimos por validación cruzada. Se han añadido todas las funciones al script utils.py. Se han eliminado todos los ficheros de datos ya que se acceden a ellos vía API.

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import streamlit as st
-
-from aemet import Aemet
-import pandas as pd
-from datetime import date, timedelta
-from config import API_KEY
-
+from utils import *
 from prophet import Prophet
+from datetime import date
 from prophet.plot import plot_plotly
-from plotly import graph_objs as go
-
 import warnings
 warnings.simplefilter("ignore", category=FutureWarning)
 
@@ -20,87 +14,123 @@ st.markdown(
 	""", unsafe_allow_html=True
 	)
 
-st.title('Weather data forecast in Valencia')
-
-stations = ('8058X', '8325X', '8309X', '8414A', '8416Y', '8416', '8293X')
-
 st.sidebar.title('Settings')
 st.sidebar.markdown('---')
-station = st.sidebar.selectbox('**Select station for prediction**', stations, index=None)
-st.sidebar.markdown('---')
-fechaini = st.sidebar.date_input('**Select a start date**', date(2018,1,1))
-fechafin = st.sidebar.date_input('**Select an end date**', date(2022,12,31))
-st.sidebar.markdown('---')
-n_days = st.sidebar.slider('**Days of prediction:**',min_value=1, max_value=60, value=40)
+
+with st.sidebar.expander('**Station**'):
+	station = st.selectbox('**Select station for prediction**', stations, index=None)
+
+with st.sidebar.expander('**Date**'):
+	fechaini = st.date_input('**Select a start date**', date(2018,1,1))
+	fechafin = st.date_input('**Select an end date**', date(2022,12,31))
+
+n_days = st.sidebar.slider('**Days of prediction:**',min_value=1, max_value=365, value=200)
+tuning = st.sidebar.checkbox('Hyperparameter tuning')
+forecast = st.sidebar.button('Make forecast', use_container_width=True)
+
+# Aquí meter un selector de hiperparámetros para seleccionarlos manualmente si no se elige 'tuning'
+
+st.title('Weather data forecast in Valencia')
+st.markdown('---')
 
 if station:
-	forecast = st.sidebar.button('Make forecast')
 
-def plot_raw_data():
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=df['fecha'], y=df['tmed'], name="tmed"))
-	fig.layout.update(title_text='Time Series data', xaxis_rangeslider_visible=True)
-	st.plotly_chart(fig)
+	if forecast:
 
-# Comentado porque al modificar fechas se guarda la primera que se selecciona
-#@st.cache_data
-def load_data():
-	aemet = Aemet(api_key=API_KEY)
-	data = aemet.get_valores_climatologicos_diarios(fechaini=fechaini.strftime("%Y-%m-%dT%H:%M:%SUTC"),
-												    fechafin=fechafin.strftime("%Y-%m-%dT%H:%M:%SUTC"),
-													estacion=station)
-	df = pd.DataFrame(data)
-	df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
-	numeric_columns = ['tmed','tmin','tmax','velmedia','racha','sol','presMax','presMin']
-	for column in numeric_columns:
-		df[column] = df[column].str.replace(',', '.').astype(float)
-	return df
+		if tuning:
 
-# Comentado porque al modificar fechas se guarda la primera que se selecciona
-#@st.cache_data
-def load_new_data():
-	aemet = Aemet(api_key=API_KEY)
-	data = aemet.get_valores_climatologicos_diarios(fechaini=(fechafin + timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SUTC"),
-												    fechafin=(fechafin + timedelta(days=n_days)).strftime("%Y-%m-%dT%H:%M:%SUTC"),
-												    estacion=station)
-	df_new = pd.DataFrame(data)
-	df_new['fecha'] = pd.to_datetime(df_new['fecha'], format='%Y-%m-%d')
-	numeric_columns = ['tmed','tmin','tmax','velmedia','racha','sol','presMax','presMin']
-	for column in numeric_columns:
-		df_new[column] = df_new[column].str.replace(',', '.').astype(float)
-	return df_new
+			with st.spinner(text='Loading data...'):
+				df = load_data(station, fechaini, fechafin)
+			st.success('El dataframe df ha sido cargado satisfactoriamente!')
 
-if forecast:
+			st.subheader('Raw data')
+			st.markdown('---')
+			st.dataframe(df.tail(10), use_container_width=True)
 
-	data_load_state = st.text('Loading data...')
-	df = load_data()
-	data_load_state.text('Loading data... done!')
+			df_train = df[['fecha','tmed']]
+			df_train = df_train.rename(columns={'fecha': 'ds', 'tmed': 'y'})
 
-	df_train = df[['fecha','tmed']]
-	df_train = df_train.rename(columns={"fecha": "ds", "tmed": "y"})
+			plot_raw_data(df)
 
-	st.subheader('Raw data')
-	st.dataframe(df.tail(10))
+			# Carga de los datos de test
+			df_test = load_new_data(station, fechafin, n_days)
 
-	st.subheader('Raw new data')
-	df_new = load_new_data()
-	st.dataframe(df_new)
+			with st.spinner(text='Calculando hiperparámetros óptimos...'):
+				best_params = hyperparameter_tuning(df_train)
+			st.success('Hiperparámetros óptimos calculados satisfactoriamente!')
 
-	plot_raw_data()
+			model = Prophet(**best_params)
+			model.fit(df_train)
+			future = model.make_future_dataframe(periods=n_days)
+			forecast = model.predict(future)
 
-	model = Prophet()
-	model.fit(df_train)
-	future = model.make_future_dataframe(periods=n_days)
-	forecast = model.predict(future)
+			st.subheader('Forecast data')
+			st.markdown('---')
+			st.dataframe(forecast[['ds', 'trend_lower', 'trend', 'trend_upper', 'yhat_lower', 'yhat', 'yhat_upper']].tail(n_days), use_container_width=True)
 
-	st.subheader('Forecast data')
-	st.dataframe(forecast[['ds', 'trend_lower', 'trend', 'trend_upper', 'yhat_lower', 'yhat', 'yhat_upper']].tail(n_days))
-		
-	st.write(f'Forecast plot for {n_days} days')
-	fig1 = plot_plotly(model, forecast)
-	st.plotly_chart(fig1)
+			st.write(f'Forecast plot for {n_days} days')
+			fig1 = plot_plotly(model, forecast)
+			#fig1.update_traces(line=dict(color='rgba(110, 135, 255, 0.5)')) # color de la traza
+			fig1.update_traces(marker=dict(color='rgb(110, 135, 255)'), selector=dict(mode='markers')) # color puntos observaciones reales
+			fig1.update_traces(fillcolor='rgba(110, 135, 255, 0.3)') # color bandas de confianza
+			st.plotly_chart(fig1)
 
-	# Sustituir esto por una tabla resumen con las métricas más relevantes
-	st.subheader("Forecast components")
-	fig2 = model.plot_components(forecast)
-	st.write(fig2)
+			st.subheader('Metrics')
+			st.markdown('---')
+			y_pred = forecast['yhat'].tail(n_days).reset_index(drop=True)
+			y_test = df_test['tmed'].head(n_days)
+
+			df_errors = calculate_metrics(y_pred, y_test)
+			st.dataframe(df_errors, use_container_width=True)
+
+			plot_forecast(n_days, forecast, y_test, y_pred)
+
+		else:
+
+			with st.spinner(text='Loading data...'):
+				df = load_data(station, fechaini, fechafin)
+			st.success('El dataframe df ha sido cargado satisfactoriamente!')
+
+			st.subheader('Raw data')
+			st.markdown('---')
+			st.dataframe(df.tail(10), use_container_width=True)
+
+			df_train = df[['fecha','tmed']]
+			df_train = df_train.rename(columns={'fecha': 'ds', 'tmed': 'y'})
+
+			plot_raw_data(df)
+
+			# Carga de los datos de test
+			df_new = load_new_data(station, fechafin, n_days)
+
+			model = Prophet()
+			model.fit(df_train)
+			future = model.make_future_dataframe(periods=n_days)
+			forecast = model.predict(future)
+
+			st.subheader('Forecast data')
+			st.markdown('---')
+			st.dataframe(forecast[['ds', 'trend_lower', 'trend', 'trend_upper', 'yhat_lower', 'yhat', 'yhat_upper']].tail(n_days), use_container_width=True)
+
+			st.write(f'Forecast plot for {n_days} days')
+			fig1 = plot_plotly(model, forecast)
+			#fig1.update_traces(line=dict(color='rgba(110, 135, 255, 0.5)')) # color de la traza
+			fig1.update_traces(marker=dict(color='rgb(110, 135, 255)'), selector=dict(mode='markers')) # color puntos observaciones reales
+			fig1.update_traces(fillcolor='rgba(110, 135, 255, 0.3)') # color bandas de confianza
+			st.plotly_chart(fig1)
+
+			st.subheader('Metrics')
+			st.markdown('---')
+			y_pred = forecast['yhat'].tail(n_days).reset_index(drop=True)
+			y_test = df_new['tmed'].head(n_days)
+
+			df_errors = calculate_metrics(y_pred, y_test)
+			st.dataframe(df_errors, use_container_width=True)
+
+			plot_forecast(n_days, forecast, y_test, y_pred)
+
+	else:
+		st.info('Select if you want to adjust the hyperparameters automatically.')
+
+else:
+	st.info('Please, select a station.')
