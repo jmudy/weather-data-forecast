80060738a5c10c674be6908aa7012deaaf21915c
jmudy <62428273+jmudy@users.noreply.github.com>
2023-10-27 21:47:39 +0200

    Reformateo con libreria black

--- app.py
+++ app.py
@@ -4,144 +4,190 @@ from prophet import Prophet
 from datetime import date
 from prophet.plot import plot_plotly
 import warnings
+
 warnings.simplefilter("ignore", category=FutureWarning)
 
 st.markdown(
-	"""
+    """
 	<style>
 		section.main > div {max-width:70rem}
 	</style>
-	""", unsafe_allow_html=True
-	)
+	""",
+    unsafe_allow_html=True,
+)
 
-st.sidebar.title('Settings')
-st.sidebar.markdown('---')
+st.sidebar.title("Settings")
+st.sidebar.markdown("---")
 
-with st.sidebar.expander('**Station**'):
-	station = st.selectbox('**Select station for prediction**', list(stations.values()), index=None)
-	if station:
-		station_code = [code for code, name in stations.items() if name == station][0]
+with st.sidebar.expander("**Station**"):
+    station = st.selectbox(
+        "**Select station for prediction**", list(stations.values()), index=None
+    )
+    if station:
+        station_code = [code for code, name in stations.items() if name == station][0]
 
-with st.sidebar.expander('**Date**'):
-	fechaini = st.date_input('**Select a start date**', date(2018,1,1))
-	fechafin = st.date_input('**Select an end date**', date(2022,12,31))
+with st.sidebar.expander("**Date**"):
+    fechaini = st.date_input("**Select a start date**", date(2018, 1, 1))
+    fechafin = st.date_input("**Select an end date**", date(2022, 12, 31))
 
-n_days = st.sidebar.slider('**Days of prediction:**',min_value=1, max_value=365, value=290)
+n_days = st.sidebar.slider(
+    "**Days of prediction:**", min_value=1, max_value=365, value=290
+)
 
-tuning = st.sidebar.checkbox('Hyperparameter tuning')
+tuning = st.sidebar.checkbox("Hyperparameter tuning")
 
 if not tuning:
-	changepoint_prior_scale = st.sidebar.number_input('Insert changepoint_prior_scale value', value=0.05)
-	seasonality_prior_scale = st.sidebar.number_input('Insert seasonality_prior_scale value', value=10.0)
-	holidays_prior_scale = st.sidebar.number_input('Insert holidays_prior_scale value', value=10.0)
-	seasonality_mode = st.sidebar.selectbox('Select seasonality_mode value', ('additive', 'multiplicative'), index=0)
+    changepoint_prior_scale = st.sidebar.number_input(
+        "Insert changepoint_prior_scale value", value=0.05
+    )
+    seasonality_prior_scale = st.sidebar.number_input(
+        "Insert seasonality_prior_scale value", value=10.0
+    )
+    holidays_prior_scale = st.sidebar.number_input(
+        "Insert holidays_prior_scale value", value=10.0
+    )
+    seasonality_mode = st.sidebar.selectbox(
+        "Select seasonality_mode value", ("additive", "multiplicative"), index=0
+    )
 
-forecast = st.sidebar.button('Make forecast', use_container_width=True)
+forecast = st.sidebar.button("Make forecast", use_container_width=True)
 
-st.title('Weather data forecast in Valencia')
-st.markdown('---')
+st.title("Weather data forecast in Valencia")
+st.markdown("---")
 
 if station:
+    if forecast:
+        if tuning:
+            with st.spinner(text="Loading data..."):
+                df = load_data(station_code, fechaini, fechafin)
+            st.success("El dataframe df ha sido cargado satisfactoriamente!")
 
-	if forecast:
+            st.subheader("Raw data")
+            st.markdown("---")
+            st.dataframe(df.tail(10), use_container_width=True)
 
-		if tuning:
+            df_train = df[["fecha", "tmed"]]
+            df_train = df_train.rename(columns={"fecha": "ds", "tmed": "y"})
 
-			with st.spinner(text='Loading data...'):
-				df = load_data(station_code, fechaini, fechafin)
-			st.success('El dataframe df ha sido cargado satisfactoriamente!')
+            plot_raw_data(df)
 
-			st.subheader('Raw data')
-			st.markdown('---')
-			st.dataframe(df.tail(10), use_container_width=True)
+            # Carga de los datos de test
+            df_test = load_new_data(station_code, fechafin, n_days)
 
-			df_train = df[['fecha','tmed']]
-			df_train = df_train.rename(columns={'fecha': 'ds', 'tmed': 'y'})
+            with st.spinner(text="Calculando hiperparámetros óptimos..."):
+                best_params = hyperparameter_tuning(df_train)
+            st.success("Hiperparámetros óptimos calculados satisfactoriamente!")
 
-			plot_raw_data(df)
+            model = Prophet(**best_params)
+            model.fit(df_train)
+            future = model.make_future_dataframe(periods=n_days)
+            forecast = model.predict(future)
 
-			# Carga de los datos de test
-			df_test = load_new_data(station_code, fechafin, n_days)
+            st.subheader("Forecast data")
+            st.markdown("---")
+            st.dataframe(
+                forecast[
+                    [
+                        "ds",
+                        "trend_lower",
+                        "trend",
+                        "trend_upper",
+                        "yhat_lower",
+                        "yhat",
+                        "yhat_upper",
+                    ]
+                ].tail(n_days),
+                use_container_width=True,
+            )
 
-			with st.spinner(text='Calculando hiperparámetros óptimos...'):
-				best_params = hyperparameter_tuning(df_train)
-			st.success('Hiperparámetros óptimos calculados satisfactoriamente!')
+            st.write(f"Forecast plot for {n_days} days")
+            fig1 = plot_plotly(model, forecast)
+            # fig1.update_traces(line=dict(color='rgba(110, 135, 255, 0.5)')) # color de la traza
+            fig1.update_traces(
+                marker=dict(color="rgb(110, 135, 255)"), selector=dict(mode="markers")
+            )  # color puntos observaciones reales
+            fig1.update_traces(
+                fillcolor="rgba(110, 135, 255, 0.3)"
+            )  # color bandas de confianza
+            st.plotly_chart(fig1)
 
-			model = Prophet(**best_params)
-			model.fit(df_train)
-			future = model.make_future_dataframe(periods=n_days)
-			forecast = model.predict(future)
+            st.subheader("Metrics")
+            st.markdown("---")
+            y_pred = forecast["yhat"].tail(n_days).reset_index(drop=True)
+            y_test = df_test["tmed"].head(n_days)
 
-			st.subheader('Forecast data')
-			st.markdown('---')
-			st.dataframe(forecast[['ds', 'trend_lower', 'trend', 'trend_upper', 'yhat_lower', 'yhat', 'yhat_upper']].tail(n_days), use_container_width=True)
+            df_errors = calculate_metrics(y_pred, y_test)
+            st.dataframe(df_errors, use_container_width=True)
 
-			st.write(f'Forecast plot for {n_days} days')
-			fig1 = plot_plotly(model, forecast)
-			#fig1.update_traces(line=dict(color='rgba(110, 135, 255, 0.5)')) # color de la traza
-			fig1.update_traces(marker=dict(color='rgb(110, 135, 255)'), selector=dict(mode='markers')) # color puntos observaciones reales
-			fig1.update_traces(fillcolor='rgba(110, 135, 255, 0.3)') # color bandas de confianza
-			st.plotly_chart(fig1)
+            plot_forecast(n_days, forecast, y_test, y_pred)
 
-			st.subheader('Metrics')
-			st.markdown('---')
-			y_pred = forecast['yhat'].tail(n_days).reset_index(drop=True)
-			y_test = df_test['tmed'].head(n_days)
+        else:
+            with st.spinner(text="Loading data..."):
+                df = load_data(station_code, fechaini, fechafin)
+            st.success("El dataframe df ha sido cargado satisfactoriamente!")
 
-			df_errors = calculate_metrics(y_pred, y_test)
-			st.dataframe(df_errors, use_container_width=True)
+            st.subheader("Raw data")
+            st.markdown("---")
+            st.dataframe(df.tail(10), use_container_width=True)
 
-			plot_forecast(n_days, forecast, y_test, y_pred)
+            df_train = df[["fecha", "tmed"]]
+            df_train = df_train.rename(columns={"fecha": "ds", "tmed": "y"})
 
-		else:
+            plot_raw_data(df)
 
-			with st.spinner(text='Loading data...'):
-				df = load_data(station_code, fechaini, fechafin)
-			st.success('El dataframe df ha sido cargado satisfactoriamente!')
+            # Carga de los datos de test
+            df_new = load_new_data(station_code, fechafin, n_days)
 
-			st.subheader('Raw data')
-			st.markdown('---')
-			st.dataframe(df.tail(10), use_container_width=True)
+            model = Prophet(
+                changepoint_prior_scale=changepoint_prior_scale,
+                seasonality_prior_scale=seasonality_prior_scale,
+                holidays_prior_scale=holidays_prior_scale,
+                seasonality_mode=seasonality_mode,
+            )
+            model.fit(df_train)
+            future = model.make_future_dataframe(periods=n_days)
+            forecast = model.predict(future)
 
-			df_train = df[['fecha','tmed']]
-			df_train = df_train.rename(columns={'fecha': 'ds', 'tmed': 'y'})
+            st.subheader("Forecast data")
+            st.markdown("---")
+            st.dataframe(
+                forecast[
+                    [
+                        "ds",
+                        "trend_lower",
+                        "trend",
+                        "trend_upper",
+                        "yhat_lower",
+                        "yhat",
+                        "yhat_upper",
+                    ]
+                ].tail(n_days),
+                use_container_width=True,
+            )
 
-			plot_raw_data(df)
+            st.write(f"Forecast plot for {n_days} days")
+            fig1 = plot_plotly(model, forecast)
+            # fig1.update_traces(line=dict(color='rgba(110, 135, 255, 0.5)')) # color de la traza
+            fig1.update_traces(
+                marker=dict(color="rgb(110, 135, 255)"), selector=dict(mode="markers")
+            )  # color puntos observaciones reales
+            fig1.update_traces(
+                fillcolor="rgba(110, 135, 255, 0.3)"
+            )  # color bandas de confianza
+            st.plotly_chart(fig1)
 
-			# Carga de los datos de test
-			df_new = load_new_data(station_code, fechafin, n_days)
+            st.subheader("Metrics")
+            st.markdown("---")
+            y_pred = forecast["yhat"].tail(n_days).reset_index(drop=True)
+            y_test = df_new["tmed"].head(n_days)
 
-			model = Prophet(changepoint_prior_scale=changepoint_prior_scale,
-				            seasonality_prior_scale=seasonality_prior_scale,
-				            holidays_prior_scale=holidays_prior_scale,
-				            seasonality_mode=seasonality_mode)
-			model.fit(df_train)
-			future = model.make_future_dataframe(periods=n_days)
-			forecast = model.predict(future)
+            df_errors = calculate_metrics(y_pred, y_test)
+            st.dataframe(df_errors, use_container_width=True)
 
-			st.subheader('Forecast data')
-			st.markdown('---')
-			st.dataframe(forecast[['ds', 'trend_lower', 'trend', 'trend_upper', 'yhat_lower', 'yhat', 'yhat_upper']].tail(n_days), use_container_width=True)
+            plot_forecast(n_days, forecast, y_test, y_pred)
 
-			st.write(f'Forecast plot for {n_days} days')
-			fig1 = plot_plotly(model, forecast)
-			#fig1.update_traces(line=dict(color='rgba(110, 135, 255, 0.5)')) # color de la traza
-			fig1.update_traces(marker=dict(color='rgb(110, 135, 255)'), selector=dict(mode='markers')) # color puntos observaciones reales
-			fig1.update_traces(fillcolor='rgba(110, 135, 255, 0.3)') # color bandas de confianza
-			st.plotly_chart(fig1)
-
-			st.subheader('Metrics')
-			st.markdown('---')
-			y_pred = forecast['yhat'].tail(n_days).reset_index(drop=True)
-			y_test = df_new['tmed'].head(n_days)
-
-			df_errors = calculate_metrics(y_pred, y_test)
-			st.dataframe(df_errors, use_container_width=True)
-
-			plot_forecast(n_days, forecast, y_test, y_pred)
-
-	else:
-		st.info('Select if you want to adjust the hyperparameters automatically.')
+    else:
+        st.info("Select if you want to adjust the hyperparameters automatically.")
 
 else:
-	st.info('Please, select a station.')
+    st.info("Please, select a station.")
--- utils.py
+++ utils.py
@@ -13,89 +13,138 @@ from sklearn import metrics
 stations = {
     #'8058X': 'OLIVA',
     #'8325X': 'POLINYÀ DE XÚQUER',
-    '8309X': 'UTIEL',
-    '8414A': 'VALENCIA AEROPUERTO',
+    "8309X": "UTIEL",
+    "8414A": "VALENCIA AEROPUERTO",
     #'8416Y': 'VALÈNCIA, VIVEROS',
     #'8416': 'VALÈNCIA',
-    '8293X': 'XÀTIVA',
+    "8293X": "XÀTIVA",
 }
-cutoffs = pd.to_datetime(['2018-12-31', '2019-12-31', '2020-12-31', '2021-12-31'])
+cutoffs = pd.to_datetime(["2018-12-31", "2019-12-31", "2020-12-31", "2021-12-31"])
+
 
 def plot_raw_data(df):
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=df['fecha'], y=df['tmed'], name='tmed'))
-	fig.update_layout(xaxis_title='Date',yaxis_title='Temperatura media',
-                      title='Time Series data',
-                      height=600, width=1000)
-	st.plotly_chart(fig)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df["fecha"], y=df["tmed"], name="tmed"))
+    fig.update_layout(
+        xaxis_title="Date",
+        yaxis_title="Temperatura media",
+        title="Time Series data",
+        height=600,
+        width=1000,
+    )
+    st.plotly_chart(fig)
+
 
 def plot_forecast(n_days, forecast, y_test, y_pred):
-	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=forecast['ds'].tail(n_days), y=y_test, mode='lines', name='Real'))
-	fig.add_trace(go.Scatter(x=forecast['ds'].tail(n_days), y=y_pred, mode='lines', name='Forecast', line=dict(color='orange')))
-	fig.update_layout(
-		xaxis_title='Date', yaxis_title='Temperatura media',
-		title='Facebook Prophet forecast',
-		height=600, width=1000)
-	st.plotly_chart(fig)
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(x=forecast["ds"].tail(n_days), y=y_test, mode="lines", name="Real")
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=forecast["ds"].tail(n_days),
+            y=y_pred,
+            mode="lines",
+            name="Forecast",
+            line=dict(color="orange"),
+        )
+    )
+    fig.update_layout(
+        xaxis_title="Date",
+        yaxis_title="Temperatura media",
+        title="Facebook Prophet forecast",
+        height=600,
+        width=1000,
+    )
+    st.plotly_chart(fig)
+
 
 def load_data(station_code, fechaini, fechafin):
-	aemet = Aemet(api_key=API_KEY)
-	data = aemet.get_valores_climatologicos_diarios(fechaini=fechaini.strftime('%Y-%m-%dT%H:%M:%SUTC'),
-												    fechafin=fechafin.strftime('%Y-%m-%dT%H:%M:%SUTC'),
-													estacion=station_code)
-	df = pd.DataFrame(data)
-	df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
-	numeric_columns = ['tmed','tmin','tmax','velmedia','racha','sol','presMax','presMin']
-	for column in numeric_columns:
-		df[column] = df[column].str.replace(',', '.').astype(float)
-	return df
+    aemet = Aemet(api_key=API_KEY)
+    data = aemet.get_valores_climatologicos_diarios(
+        fechaini=fechaini.strftime("%Y-%m-%dT%H:%M:%SUTC"),
+        fechafin=fechafin.strftime("%Y-%m-%dT%H:%M:%SUTC"),
+        estacion=station_code,
+    )
+    df = pd.DataFrame(data)
+    df["fecha"] = pd.to_datetime(df["fecha"], format="%Y-%m-%d")
+    numeric_columns = [
+        "tmed",
+        "tmin",
+        "tmax",
+        "velmedia",
+        "racha",
+        "sol",
+        "presMax",
+        "presMin",
+    ]
+    for column in numeric_columns:
+        df[column] = df[column].str.replace(",", ".").astype(float)
+    return df
+
 
 def load_new_data(station_code, fechafin, n_days):
-	aemet = Aemet(api_key=API_KEY)
-	data = aemet.get_valores_climatologicos_diarios(fechaini=(fechafin + timedelta(days=1)).strftime('%Y-%m-%dT%H:%M:%SUTC'),
-												    fechafin=(fechafin + timedelta(days=n_days)).strftime('%Y-%m-%dT%H:%M:%SUTC'),
-												    estacion=station_code)
-	df_new = pd.DataFrame(data)
-	df_new['fecha'] = pd.to_datetime(df_new['fecha'], format='%Y-%m-%d')
-	numeric_columns = ['tmed','tmin','tmax','velmedia','racha','sol','presMax','presMin']
-	for column in numeric_columns:
-		df_new[column] = df_new[column].str.replace(',', '.').astype(float)
-	return df_new
+    aemet = Aemet(api_key=API_KEY)
+    data = aemet.get_valores_climatologicos_diarios(
+        fechaini=(fechafin + timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SUTC"),
+        fechafin=(fechafin + timedelta(days=n_days)).strftime("%Y-%m-%dT%H:%M:%SUTC"),
+        estacion=station_code,
+    )
+    df_new = pd.DataFrame(data)
+    df_new["fecha"] = pd.to_datetime(df_new["fecha"], format="%Y-%m-%d")
+    numeric_columns = [
+        "tmed",
+        "tmin",
+        "tmax",
+        "velmedia",
+        "racha",
+        "sol",
+        "presMax",
+        "presMin",
+    ]
+    for column in numeric_columns:
+        df_new[column] = df_new[column].str.replace(",", ".").astype(float)
+    return df_new
+
 
 def hyperparameter_tuning(df_train):
-	param_grid = {
-		'changepoint_prior_scale': [0.001, 0.01, 0.1, 0.5],
-		'seasonality_prior_scale': [0.01, 0.1, 1.0, 10.0],
-		'holidays_prior_scale': [0.01, 10],
-		'seasonality_mode': ['additive', 'multiplicative']
-	}
+    param_grid = {
+        "changepoint_prior_scale": [0.001, 0.01, 0.1, 0.5],
+        "seasonality_prior_scale": [0.01, 0.1, 1.0, 10.0],
+        "holidays_prior_scale": [0.01, 10],
+        "seasonality_mode": ["additive", "multiplicative"],
+    }
 
-	all_params = [dict(zip(param_grid.keys(), v)) for v in itertools.product(*param_grid.values())]
-	rmses = []
-	progress_bar = st.progress(0)
+    all_params = [
+        dict(zip(param_grid.keys(), v)) for v in itertools.product(*param_grid.values())
+    ]
+    rmses = []
+    progress_bar = st.progress(0)
 
-	for i, params in enumerate(all_params):
-		model = Prophet(**params).fit(df_train)
-		df_cv = cross_validation(model, cutoffs=cutoffs, horizon='200 days')
-		df_p = performance_metrics(df_cv, rolling_window=1)
-		rmses.append(df_p['rmse'].values[0])
-		progress = (i+1)/len(all_params)
-		progress_bar.progress(progress)
+    for i, params in enumerate(all_params):
+        model = Prophet(**params).fit(df_train)
+        df_cv = cross_validation(model, cutoffs=cutoffs, horizon="200 days")
+        df_p = performance_metrics(df_cv, rolling_window=1)
+        rmses.append(df_p["rmse"].values[0])
+        progress = (i + 1) / len(all_params)
+        progress_bar.progress(progress)
 
-	tuning_results = pd.DataFrame(all_params)
-	tuning_results['rmse'] = rmses
-	best_params = all_params[np.argmin(rmses)]
-	return best_params
+    tuning_results = pd.DataFrame(all_params)
+    tuning_results["rmse"] = rmses
+    best_params = all_params[np.argmin(rmses)]
+    return best_params
+
 
 def calculate_metrics(y_pred, y_test):
     mae = metrics.mean_absolute_error(y_test, y_pred)
-    mape = metrics.mean_absolute_percentage_error(y_test, y_pred)*100
+    mape = metrics.mean_absolute_percentage_error(y_test, y_pred) * 100
     mse = metrics.mean_squared_error(y_test, y_pred)
     rmse = metrics.mean_squared_error(y_test, y_pred, squared=False)
-    data = {'MAE': [round(mae, 4)],
-			'MAPE (%)': [round(mape, 4)],
-			'MSE': [round(mse, 4)],
-			'RMSE': [round(rmse, 4)]}
+    data = {
+        "MAE": [round(mae, 4)],
+        "MAPE (%)": [round(mape, 4)],
+        "MSE": [round(mse, 4)],
+        "RMSE": [round(rmse, 4)],
+    }
     df_errors = pd.DataFrame(data)
     return df_errors
